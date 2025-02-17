d12e7d2ddb71a2ee5d15b0c59503a03d87b2740a
Marcus <marcusfromsweden@gmail.com>
2025-02-17 12:54:33 +0100

    added handler method for healthcheck

---
+++ cmd/api/handlers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w,
+			http.StatusText(http.StatusMethodNotAllowed),
+			http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintln(w, "I am alive!")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", healthcheck)
+const version = "1.0.0"
 
-	err := http.ListenAndServe(":4000", mux) // nil => using default router
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|test|stage|prod)")
+	flag.Parse() // parse the flags and store the values in the config struct
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	serverAddress := fmt.Sprintf(":%d", cfg.port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
+
+	logger.Printf("Starting (%s) server on %s", cfg.env, serverAddress)
+	err := http.ListenAndServe(serverAddress, mux)
 	if err != nil {
 		fmt.Println("Error starting server: ", err)
 	}
-}
-
-func healthcheck(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w,
-			http.StatusText(http.StatusMethodNotAllowed),
-			http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Fprintln(w, "I am alive!")
-	fmt.Fprintf(w, "environment: %s\n", "dev")
-	fmt.Fprintf(w, "version: %s\n", "1.0.0")
 }
