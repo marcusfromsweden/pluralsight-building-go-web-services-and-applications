50123a71c443253143d60aa397f70b1c5115ea85
Marcus <marcusfromsweden@gmail.com>
2025-02-17 15:54:32 +0100

    connected to postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq" // external package for PostgreSQL driver
 )
 
 const version = "1.0.0"
@@ -14,6 +17,7 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	dsn  string
 }
 
 type application struct {
@@ -22,9 +26,12 @@ type application struct {
 }
 
 func main() {
+
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|test|stage|prod)")
+	flag.StringVar(&cfg.dsn, "db-dsn", os.Getenv("READINGLIST_DB_DSN"), "PostgreSQL DSN")
+
 	flag.Parse() // parse the flags and store the values in the config struct
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -32,6 +39,19 @@ func main() {
 		config: cfg,
 		logger: logger,
 	}
+	logger.Println("Using DSN: ", cfg.dsn)
+
+	database, err := sql.Open("postgres", cfg.dsn)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer database.Close()
+
+	err = database.Ping()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	logger.Println("Database connection successful")
 
 	serverAddress := fmt.Sprintf(":%d", cfg.port)
 	server := &http.Server{
@@ -43,7 +63,7 @@ func main() {
 	}
 
 	logger.Printf("Starting (%s) server on %s", cfg.env, serverAddress)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		logger.Fatal(err)
 	}
